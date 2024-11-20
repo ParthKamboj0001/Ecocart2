6b1e4e4f03ae3eca10c0ab5b11370391fae14182
ParthKamboj0001 <parth.kamboj000111@gmail.com>
2024-11-20 15:14:14 +0530

    Add files via upload

---
+++ Ecocart/Ecocart/src/test/java/com/ecocart/Ecocart/EcocartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecocart.Ecocart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
